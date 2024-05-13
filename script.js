c77199ebf9726a75a175476c13990abbacb542c3
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-05-13 18:57:40 -0400

    Add initial iteration of operate function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Eamon McKeon
 // JS for calculator project
 
+let num1;
+let num2;
+let operator;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -17,3 +21,18 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "x":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+    }
+}
