6bc8a931320986333a700427f0586543d09e1472
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-05-14 18:23:06 -0400

    Add start of calculator functionality
    The calculator will populate its display with numbers punched in. Does
    not yet respond to any operators entered. Input numbers are stored as a
    key-value pair in an object named "display".

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
 // Eamon McKeon
 // JS for calculator project
 
+
+/*
+
+// DOM references to buttons
+// Numbers
+const oneButton = document.querySelector("#one");
+const twoButton = document.querySelector("#two");
+const threeButton = document.querySelector("#three");
+const fourButton = document.querySelector("#four");
+const fiveButton = document.querySelector("#five");
+const sixButton = document.querySelector("#six");
+const sevenButton = document.querySelector("#seven");
+const eightButton = document.querySelector("#eight");
+const nineButton = document.querySelector("#nine");
+const zeroButton = document.querySelector("#zero");
+// Operators
+const addButton = document.querySelector("#add");
+const subtractButton = document.querySelector("#subtract");
+const multiplyButton = document.querySelector("#multiply");
+const divideButton = document.querySelector("#divide");
+const equalsButton = document.querySelector("#equals");
+const clearButton = document.querySelector("#clear");
+//
+
+*/
+
+const displayValue = document.querySelector("#display");
+
+const buttonList = document.querySelector("#button-container");
+
+buttonList.addEventListener('click', (event) => {
+    //alert(event.target.textContent);
+    let input = event.target.textContent;
+    if (!isNaN(Number(input)) && !display["operator"]) {
+        if (display["first"]) {
+            display["first"] += input;
+        }
+        else {
+            display["first"] = input;
+        }
+        displayValue.textContent = display["first"];
+    }
+})
+
+
 let num1;
 let num2;
 let operator;
+
+let display = {};
 
 function add(num1, num2) {
     return num1 + num2;
@@ -36,3 +83,16 @@ function operate(num1, num2, operator) {
             divide(num1, num2);
     }
 }
+
+/*
+
+Create the functions that populate the display when you click 
+the number buttons. You should be storing the ‘display value’ 
+in a variable somewhere for use in the next step.
+
+*/
+
+// add event listener for button display that reads in button text content 
+// depending on button clicked, do something!
+
+// start with populating the display (for numbers)
